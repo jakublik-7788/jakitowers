085a45f99a5bed81b3eb3790b39efdbb84794bab
jakublik-7788 <jakublikgraphics@gmail.com>
2026-03-18 23:01:58 +0100

    mobile5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from "react";
 import { CustomCursor } from "./components/CustomCursor";
 import { PreLaunchOverlay } from "./components/PreLaunchOverlay";
 
@@ -39,6 +40,22 @@ const AmbientGlow = () => (
   </div>
 );
 
+// Komponent kliencki do warunkowego renderowania kursora
+const ClientCursor = () => {
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    const check = () => {
+      setIsMobile(window.matchMedia("(hover: none) and (pointer: coarse)").matches);
+    };
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, []);
+
+  return !isMobile ? <CustomCursor /> : null;
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -57,7 +74,7 @@ export default function RootLayout({
       >
         <AmbientGlow />
         <main className="relative z-10">{children}</main>
-        <CustomCursor />
+        <ClientCursor />
         {/* <PreLaunchOverlay /> */}
       </body>
     </html>
