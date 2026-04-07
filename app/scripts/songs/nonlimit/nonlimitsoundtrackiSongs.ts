ca77350c985680b40e26985320b9a3a620b2d5f4
jakublik-7788 <jakublikgraphics@gmail.com>
2026-04-07 18:41:24 +0200

    smoka

--- app/scripts/songs/nonlimit/nonlimitsoundtrackiSongs.ts
+++ app/scripts/songs/nonlimit/nonlimitsoundtrackiSongs.ts
@@ -346,7 +346,7 @@ export const nonlimitSoundtrackiSongs: Song[] = [
     day: 18,
     title: "Jak wytresować smoka",
     artist: "",
-    imageUrl: "/soundtracki/Jak wytresować smoka/jakwyresowacsmoka.jpg",
+    imageUrl: "/soundtracki/Jak wytresować smoka/jakwytresowacsmoka.jpg",
     fullAudioSrc: "/soundtracki/Jak wytresować smoka/Jak wytresować smoka.mp3",
     hint1: "Film",
     hint2: "Animowany / Przygodowy",
