6c97101d5dbfc6a815d0c7daa6ab06b958f8c9fc
jakublik-7788 <jakublikgraphics@gmail.com>
2026-04-08 23:51:40 +0200

    guesses

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClientCursorWrapper } from "./components/ClientCursorWrapper"; // nowy import
-import { PreLaunchOverlay } from "./components/PreLaunchOverlay";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
